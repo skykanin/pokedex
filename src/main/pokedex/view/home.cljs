cbaadf1184f9a9df228f9cf2214a75b8effdbcee
skykanin <3789764+skykanin@users.noreply.github.com>
2020-12-28 17:48:59 +0100

    Add react-navigation

--- src/main/pokedex/view/home.cljs
+++ src/main/pokedex/view/home.cljs
@@ -2,6 +2,8 @@
   (:require
    ["expo-constants" :as c]
    ["react-native" :as rn]
+   ["@react-navigation/native" :as n]
+   ["@react-navigation/stack" :as stack]
    [pokedex.view.card-list :refer [card-list]]
    [reagent.core :as r]))
 
@@ -10,6 +12,31 @@
           :justify-content :center
           :margin-top (.-statusBarHeight c/default)}})
 
+; (defn home []
+;   [:> rn/View {:style (:home styles)}
+;    (r/as-element [card-list])})
+
+(defn one [{:keys [navigation]}]
+  ; (println (.-navigate navigation))
+  [:> rn/View {:style {:flex 1 :align-items :center :justify-content :center}}
+   [:> rn/Text "hello from one"]
+   [:> rn/Button {:title "Go to other screen"
+                  :on-press (fn [] (. navigation navigate "Other"))}]])
+
+(defn two []
+  [:> rn/Text "hello from two"])
+
+(defonce s (stack/createStackNavigator))
+
+(defonce Navigator (.-Navigator s))
+(defonce Screen (.-Screen s))
+
 (defn home []
-  [:> rn/View {:style (:home styles)}
-   (r/as-element [card-list])])
+  [:> n/NavigationContainer
+   [:> Navigator {:initial-route-name "Home"}
+    [:> Screen {:name "Home"
+                :component (r/reactify-component one)
+                :options {:title "First"}}]
+    [:> Screen {:name "Other"
+                :component (r/reactify-component two)
+                :options {:title "Second"}}]]])
