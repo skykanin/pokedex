c51c482ae9c0b22caad19b7eae8be65bee04131c
skykanin <3789764+skykanin@users.noreply.github.com>
2021-01-19 16:54:47 +0100

    Change base-stats component rendering
    Now renders the stat names as a seperate column next to the stat bars

--- src/main/pokedex/view/info/base_stats.cljs
+++ src/main/pokedex/view/info/base_stats.cljs
@@ -8,13 +8,16 @@
 (def ^:private styles
   {:bar-view {:flex 1
               :flex-direction :row
-              :justify-content :space-between
+              :justify-content :flex-end
               :align-items :center
               :padding 10
               :background-color :pink
               :height 25
               :border-radius 5}
-   :text {:font-size 15}})
+   :bars-view {:flex 2}
+   :column-view {:flex 1}
+   :text {:font-size 15}
+   :space 5})
 
 (defn- flatten-stats
   "Flatten stat maps inside the stats vector."
@@ -27,7 +30,7 @@
   (apply max (map :base_stat stats)))
 
 (defn- calc-percent [value maximum]
-  (* 300 (/ value maximum)))
+  (str (* 100 (/ value maximum)) "%"))
 
 (defn- update-width [style-map value maximum]
   (assoc style-map :width (calc-percent value maximum)))
@@ -41,16 +44,33 @@
 
 (defn- stat-bar
   "Individual stat bar component."
-  [type highest {:keys [base_stat name]}]
+  [type highest {:keys [base_stat]}]
   [:> rn/View {:style (-> (:bar-view styles)
                           (update-width base_stat highest)
                           (modify-style type))}
-   [:> rn/Text {:style (:text styles)} (shorten (s/capitalize name))]
-   [:> rn/Text {:style (:text styles)} base_stat]])
+   [:> rn/Text {:style (:text styles)} (str base_stat)]])
 
-(defn base-stats-info [type stats]
+(defn- info-column
+  "Column for the stat names."
+  [names]
+  (into [:> rn/View {:style (:column-view styles)}]
+        (interpose [spacer-bar (* 2.2 (:space styles))]
+                   (map (fn [name]
+                          [:> rn/Text (shorten (s/capitalize name))]) names))))
+
+(defn- bar-column
+  "Column for the stat bars."
+  [type highest statvec]
+  (into [:> rn/View {:style (:bars-view styles)}]
+        (interpose [spacer-bar (:space styles)]
+                   (map (partial stat-bar type highest) statvec))))
+
+(defn base-stats-info
+  "Base stats component."
+  [type stats]
   (let [statvec (flatten-stats stats)
         highest (calc-max statvec)]
-    (into [info-container {:style {:align-items :flex-start}}]
-          (interpose [spacer-bar 5]
-                     (map (partial stat-bar type highest) statvec)))))
+    [info-container {:style {:align-items :flex-start
+                             :flex-direction :row}}
+     [info-column (map :name statvec)]
+     [bar-column type highest statvec]]))
