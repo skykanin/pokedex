4a4775762afa79ca8eef41186a94f03952e46f92
skykanin <3789764+skykanin@users.noreply.github.com>
2021-01-19 22:05:49 +0100

    Hydrate component with real flavour text data

--- src/main/pokedex/view/info/species_info.cljs
+++ src/main/pokedex/view/info/species_info.cljs
@@ -1,15 +1,18 @@
 (ns pokedex.view.info.species-info
   (:require [pokedex.view.info.info-container :refer [info-container]]
-            ["react-native" :as rn]))
+            ["react-native" :as rn]
+            [clojure.string :as s]))
 
 (def ^:private styles
   {:species-h1 {:font-size 18
                 :padding-vertical 5
                 :padding-horizontal 10}
-   :species-h2 {:font-size 16
-                :border-width 1
+   :species-h2 {:font-size 14
+                :color :#333333
+                :border-width 0
                 :border-radius 10
-                :padding 5}})
+                :padding 5
+                :text-align :center}})
 
 (def ^:private species-h1 (:species-h1 styles))
 (def ^:private species-h2 (:species-h2 styles))
@@ -19,14 +22,29 @@
          [:> rn/Text {:style t-style} title]]
         components))
 
+(defn- strip-whitespace [txt]
+  (as-> txt t
+      (s/split t #"\s")
+      (filter seq t)
+      (s/join " " t)))
+
+(defn- get-en-text
+  "Get first flavour text in English."
+  [flavour-text-entries]
+  (->> flavour-text-entries
+       (filter (comp #{"en"} :name :language))
+       (map :flavor_text)
+       first
+       strip-whitespace))
+
 (defn species-info
   "Species info component which dispays pokémon
   flavour text, height and width."
-  [weight height]
-  [info-container 
+  [flavour-text-entries weight height]
+  [info-container
    [with-title "Pokédex Entry" species-h1
     [:> rn/Text {:style species-h2}
-     "Flavour text for best effect with rolling text. You can't regret the best select of flavour text."]]
+     (get-en-text flavour-text-entries)]]
    [:> rn/View {:style {:flex 1 :flex-direction :row
                         :align-items :center}}
     [with-title "Height" species-h1
